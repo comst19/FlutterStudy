60ebb05727eda718ae5c69921ecf17e7fbf157b1
comst19 <comst19@naver.com>
2024-01-09 19:51:07 +0900

    Container, Center Widget 공부

--- flutter_widget/lib/main.dart
+++ flutter_widget/lib/main.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  // runApp(
+  //   MaterialApp(
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         actions: [
+  //           IconButton(icon: Icon(Icons.home), onPressed: (){
+  //             print('Tab!');
+  //           },),
+  //           Icon(Icons.play_arrow)
+  //         ],
+  //         title: Text('This is App bar'),
+  //       ),
+  //       body: TestWidget(),
+  //       floatingActionButton: FloatingActionButton(
+  //         child: Icon(Icons.bug_report),
+  //           onPressed: (){
+  //             print('Tab FAB!');
+  //           },
+  //       ),
+  //     )
+  //   )
+  // );
+
   runApp(
     MaterialApp(
-      home: Scaffold(
+      home : Scaffold(
         appBar: AppBar(
-          actions: [
-            IconButton(icon: Icon(Icons.home), onPressed: (){
-              print('Tab!');
-            },),
-            Icon(Icons.play_arrow)
-          ],
-          title: Text('This is App bar'),
+          title: Text(
+            'Study to Container'
+          ),
         ),
-        body: TestWidget(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.bug_report),
-            onPressed: (){
-              print('Tab FAB!');
-            },
-        ),
+        body: CustomContainer(),
       )
     )
   );
@@ -41,5 +54,37 @@ class TestWidget extends StatelessWidget {
           ),
       ),
       );
+  }
+}
+
+
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 300,
+        height: 300,
+        padding: EdgeInsets.fromLTRB(10, 12, 10, 12),
+        decoration: BoxDecoration(
+          // boxdecoration이 있을 경우 외부에 color x
+          color: Color(0xFF9DDDE5),
+          border: Border.all(color: Colors.red, width: 5, style: BorderStyle.solid),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(color : Colors.black.withOpacity(0.3), offset: Offset(6, 6), blurRadius: 10, spreadRadius: 10),
+            BoxShadow(color : Colors.blue.withOpacity(0.3), offset: Offset(-6, -6), blurRadius: 10, spreadRadius: 10)
+
+          ]
+        ),
+        // margin: EdgeInsets.symmetric(vertical: 24, horizontal: 10),
+        child: Center(child: Container(
+          color: Colors.yellow,
+            child: Text('Hello Container'))
+        ),
+      ),
+    );
   }
 }
