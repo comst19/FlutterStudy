d307ef159af06ef9593a40388c33bbf159e15dab
comst19 <comst19@naver.com>
2024-01-17 01:33:01 +0900

    flutter_InstaAppClone - 앱바 구현

---
+++ flutter_instagram/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const InstaCloneApp());
+}
+
+class InstaCloneApp extends StatelessWidget {
+  const InstaCloneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          colorScheme: const ColorScheme.light(
+              primary: Colors.white, secondary: Colors.black),
+          useMaterial3: true),
+      home: const InstaCloneHome(),
+    );
+  }
+}
+
+class InstaCloneHome extends StatelessWidget {
+  const InstaCloneHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Instagram',
+          style: GoogleFonts.lobsterTwo(
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.favorite_outline,
+              size: 32,
+            ),
+            onPressed: () {
+              print('Tab favorite');
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.paperplane,
+              size: 32,
+            ),
+            onPressed: () {
+              print('Tap paperplane');
+            },
+          )
+        ],
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
